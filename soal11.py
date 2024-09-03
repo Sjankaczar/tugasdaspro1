2bf89e7b521944e8d84a69fad6de00c29db53ef0
Sjankaczar <capriodemaestro@gmail.com>
2024-09-03 11:21:33 +0700

    Update soal11.py

---
+++ soal11.py
@@ -0,0 +1,7 @@
+#triple pythagoras
+m = float(input("masukkan nilai m ... "))
+n = float(input("masukkan nilai n ... "))
+sisi1 = m**2 - n**2
+sisi2 = 2 * m * n
+hipotenusa = m**2 + n**2
+print("triple pythagoras dari nilai m dan n adalah: \nsisi 1 =", abs(sisi1), "\nsisi 2 =", abs(sisi2), "\nhipotenusa =", hipotenusa)
